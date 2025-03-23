0eb05a2014307e304f17a07d593c40530ca6c6c1
Omotayo Ayobami Oyedele <divineayobami179@gmail.com>
2025-03-23 22:27:48 +0100

    commit

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -24,10 +24,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const navMenu = document.getElementById('nav-menu');
   
     hamburger.addEventListener('click', () => {
-      hamburger.classList.toggle('active');
-      navMenu.classList.toggle('active');
-      const ariaExpanded = navMenu.classList.contains('active') ? 'true' : 'false';
-      navMenu.setAttribute('aria-expanded', ariaExpanded);
+        hamburger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+        const ariaExpanded = navMenu.classList.contains('active') ? 'true' : 'false';
+        navMenu.setAttribute('aria-expanded', ariaExpanded);
     });
-  });
-  
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Static values for temperature (in Celsius) and wind speed (in km/h)
+    const temperature = 5; // e.g., 5°C
+    const windSpeed = 10; // e.g., 10 km/h
+
+    // Update weather information
+    updateWeatherInformation(temperature, windSpeed);
+
+    function calculateWindChill(temp, wind) {
+        return 13.12 + 0.6215 * temp - 11.37 * Math.pow(wind, 0.16) + 0.3965 * temp * Math.pow(wind, 0.16);
+    }
+
+    function updateWeatherInformation(temp, wind) {
+        let windChillFactor = "N/A";
+        if (temp <= 10 && wind > 4.8) {
+            windChillFactor = calculateWindChill(temp, wind).toFixed(2) + " °C";
+        }
+
+        document.querySelector(".weather").innerHTML += `<p>Windchill: ${windChillFactor}</p>`;
+    }
+});
